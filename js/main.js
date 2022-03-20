9aedd271e33cf5fa5fc5245944cf8aea1b009309
Himanshu <himanshu.yadav1@jungleworks.com>
2022-03-20 20:32:33 +0530

    1st page done

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 // select loader hear 
 const loader = document.querySelector('#loader');
+const main = document.querySelector('.main')
 //Hide Loader 
 function init() {
     setTimeout(() => {
-        loader.classList.remove('show');
-        loader.classList.add('hide');
-        console.log("here")
-        console.log(loader.classList)
+        // loader.classList.remove('show');
+        // loader.classList.add('hide');
+        loader.style.opacity = 0;
+        loader.style.display = 'none';
+
+        main.style.display = 'block';
+        setTimeout(()=>(main.style.opacity = 1),50)
     }, 2000)
 }
+init();
 
