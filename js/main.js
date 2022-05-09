c89572a43aaddce9db33384b5f633046c00e96a8
Himanshu <himanshu.yadav1@jungleworks.com>
2022-05-09 14:02:27 +0530

    animation changes

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ const sidebarMenu = document.querySelector('.sidebar__menu')
 const hamburgerIcon = document.getElementById('hamburger');
 const sidebarDisplay = document.getElementById('sidebar');
 const closeBtn = document.getElementById('close-menu');
-console.log(hamburgerIcon)
+const blurClass = document.querySelectorAll('.blur-class');
+console.log(blurClass)
 //Hide Loader 
 function init() {
     setTimeout(() => {
@@ -22,10 +23,22 @@ init();
 hamburgerIcon.addEventListener('click', (event) => {
     sidebarDisplay.style.display = "flex";
     sidebarMenu.style.display = "flex"
+    blurClass.forEach((element) => {
+        element.style.filter = "blur(4px)"
+    })
+    sidebarDisplay.style.animation = "slide-left 0.75s cubic-bezier(0.250, 0.460, 0.450, 0.940) both";
+
+
 })
 closeBtn.addEventListener('click', (event) => {
-    sidebarDisplay.style.display = "none";
-    sidebarMenu.style.display = "none"
+    sidebarDisplay.style.animation = "slide-right 0.75s cubic-bezier(0.250, 0.460, 0.450, 0.940) both";
+    blurClass.forEach((element) => {
+        setTimeout(() => {
+            element.style.filter = "blur(0px)"
+            sidebarDisplay.style.display = "none";
+            sidebarMenu.style.display = "none"
+        }, 750)
+    })
 })
 function sidebarDisplayfn() {
 
